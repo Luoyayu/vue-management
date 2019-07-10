e654abe18d6e6c044468e56ccc1d931bcc4faa71
Tan-Qian <648936463@qq.com>
2019-07-10 14:22:05 +0800

    update qjgl

--- src/components/qjgl.vue
+++ src/components/qjgl.vue
@@ -1,199 +1,213 @@
 <template>
-    <v-app id="qjgl-main">
-        <Navigation></Navigation>
+  <v-app id="qjgl-main">
+    <Navigation></Navigation>
 
-        <v-parallax
-                height="100%"
-                src="https://i.loli.net/2019/07/08/5d22fe520e42938158.jpg"
-                id="qjgl-parallax"
+    <v-parallax
+      height="100%"
+      src="https://i.loli.net/2019/07/08/5d22fe520e42938158.jpg"
+      id="qjgl-parallax"
+    >
+      <div id="leaveedit" style="margin-top: 50px">
+        <v-toolbar flat color="white">
+          <v-toolbar-title>员工请假审批</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn color="primary" dark @click="all_agree">
+            同意
+            <v-icon dark right>check_circle</v-icon>
+          </v-btn>
+          <v-btn color="primary" v-if="flag===true" dark @click="upload">
+            提交
+            <v-icon dark right>send</v-icon>
+          </v-btn>
+          <v-btn disabled v-if="flag===false">
+            请至少审批一项内容
+            <v-icon dark right>not_interested</v-icon>
+          </v-btn>
+        </v-toolbar>
+
+        <v-data-table
+          v-model="selected"
+          select-all
+          :headers="headers"
+          :items="tempobjects"
+          item-key="wk_number"
+          class="elevation-1"
         >
-            <div id="leaveedit" style="margin-top: 50px">
-                <v-toolbar flat color="white">
-                    <v-toolbar-title>员工请假审批</v-toolbar-title>
-                    <v-spacer></v-spacer>
-                    <v-btn color="primary" dark @click="all_agree">
-                        同意
-                        <v-icon dark right>check_circle</v-icon>
-                    </v-btn>
-                    <v-btn color="primary" v-if="flag===true" dark @click="upload">
-                        提交
-                        <v-icon dark right>send</v-icon>
-                    </v-btn>
-                    <v-btn disabled v-if="flag===false">
-                        请至少审批一项内容
-                        <v-icon dark right>not_interested</v-icon>
-                    </v-btn>
-                </v-toolbar>
+          <template v-slot:items="props">
+            <td>
+              <v-checkbox v-model="props.selected" primary hide-details></v-checkbox>
+            </td>
+            <td class="text-xs-left">{{ props.item.wk_number }}</td>
+            <td class="text-xs-left">{{ props.item.show_date }}</td>
+            <td class="text-xs-left">{{ props.item.qj_reason }}</td>
 
-                <v-data-table
-                        v-model="selected"
-                        select-all
-                        :headers="headers"
-                        :items="tempobjects"
-                        item-key="wk_number"
-                        class="elevation-1"
-                >
-                    <template v-slot:items="props">
-                        <td>
-                            <v-checkbox v-model="props.selected" primary hide-details></v-checkbox>
-                        </td>
-                        <td class="text-xs-left">{{ props.item.wk_number }}</td>
-                        <td class="text-xs-left">{{ props.item.show_date }}</td>
-                        <td class="text-xs-left">{{ props.item.qj_reason }}</td>
+            <td class="text-xs-left" v-if="props.item.qj_state===0">未审核</td>
+            <td class="text-xs-left" v-if="props.item.qj_state===1">审核通过</td>
+            <td class="text-xs-left" v-if="props.item.qj_state===2">审核未通过</td>
 
-                        <td class="text-xs-left" v-if="props.item.qj_state===0">未审核</td>
-                        <td class="text-xs-left" v-if="props.item.qj_state===1">审核通过</td>
-                        <td class="text-xs-left" v-if="props.item.qj_state===2">审核未通过</td>
-
-                        <td class="text-xs-left">
-                            <v-btn color="info" @click="showDialog=true">查看图片</v-btn>
-                        </td>
-                        <td class="text-xs-left">
-                            <v-btn color="success" @click="change(1,props.item)" v-if="props.item.qj_state===0">
-                                同意
-                                <v-icon dark right>thumb_up</v-icon>
-                            </v-btn>
-                            <v-btn color="error" @click="change(2, props.item)" v-if="props.item.qj_state===0">
-                                不同意
-                                <v-icon dark right>thumb_down</v-icon>
-                            </v-btn>
-                            <v-btn flat disabled v-if="props.item.qj_state!==0">已审批</v-btn>
-                        </td>
-                    </template>
-                </v-data-table>
-            </div>
-
-        </v-parallax>
-    </v-app>
+            <td class="text-xs-left">
+              <v-btn color="info" @click="showDialog=true">查看图片</v-btn>
+            </td>
+            <td class="text-xs-left">
+              <v-btn color="success" @click="change(1,props.item)" v-if="props.item.qj_state===0">
+                同意
+                <v-icon dark right>thumb_up</v-icon>
+              </v-btn>
+              <v-btn color="error" @click="change(2, props.item)" v-if="props.item.qj_state===0">
+                不同意
+                <v-icon dark right>thumb_down</v-icon>
+              </v-btn>
+              <v-btn flat disabled v-if="props.item.qj_state!==0">已审批</v-btn>
+            </td>
+          </template>
+        </v-data-table>
+      </div>
+    </v-parallax>
+  </v-app>
 </template>
 
 <script>
-    import Navigation from "./Navigation";
-    import {APIUpdateQingjiaState, APIGetAllQingjiaByDpNumber} from "../../global";
-    import axios from 'axios';
+import Navigation from "./Navigation";
+import {
+  APIUpdateQingjiaState,
+  APIGetAllQingjiaByDpNumber
+} from "../../global";
+import axios from "axios";
 
+export default {
+  name: "qjgl",
+  components: { Navigation },
+  data() {
+    return {
+      sendArray: [], //sendArray.lenth=this.lenth
+      showDialog: false,
+      useable: false,
+      tempobjects: [],
+      objects: [],
+      flag: false,
+      selected: [],
+      headers: [
+        { text: "员工号", align: "left", value: "wk_number" },
+        { text: "请假时间", align: "left", value: "qj_date" },
+        { text: "请假理由", align: "left", value: "qj_reason" },
+        { text: "请假状态", align: "left", value: "qj_state" },
+        { text: "附件", align: "left", value: "dasd" },
+        { text: "审查", align: "left", value: "adassa" }
+      ]
+    };
+  },
 
-    export default {
-        name: "qjgl",
-        components: {Navigation},
-        data() {
-            return {
-                returnobjects_: {
-                    wk_number: null,
-                    qj_reason: null,
-                    qj_date: null,
-                    qj_state: null,
-                },
+  methods: {
+    back() {
+      this.$router.replace("/leave");
+    },
 
-                showDialog: false,
-                useable: false,
-                tempobjects: [],
-                objects: [],
-                flag: false,
-                selected: [],
-                headers: [
-                    {text: "员工号", align: "left", value: "wk_number"},
-                    {text: "请假时间", align: "left", value: "qj_date"},
-                    {text: "请假理由", align: "left", value: "qj_reason"},
-                    {text: "请假状态", align: "left", value: "qj_state"},
-                    {text: "附件", align: "left", value: 'dasd'},
-                    {text: "审查", align: "left", value: 'adassa'}
-                ]
-            };
-        },
+    lookimg() {
+      this.$router.push("/img");
+    },
 
-        methods: {
-            back() {
-                this.$router.replace("/leave");
-            },
+    change(newstatus, obj) {
+      var returnobjects_ = {
+        wk_number: null,
+        qj_date: null,
+        permission: null
+      };
+      const index = this.tempobjects.indexOf(obj);
+      this.tempobjects[index].qj_state = newstatus;
+      returnobjects_.wk_number = this.tempobjects[index].wk_number;
+      returnobjects_.qj_date = this.tempobjects[index].qj_date;
+      returnobjects_.permission = this.tempobjects[index].qj_state;
+      this.flag = true;
+      this.sendArray.splice(index, 0, returnobjects_);
+      // eslint-disable-next-line
+      console.log(this.sendArray);
+    },
 
-            lookimg() {
-                this.$router.push("/img");
-            },
-
-            change(newstatus, obj) {
-                const index = this.objects.indexOf(obj);
-                this.objects[index].qj_state = newstatus;
-                this.returnobjects_.wk_number = this.objects[index].wk_number;
-                this.returnobjects_.qj_reason = this.objects[index].qj_reason;
-                this.returnobjects_.qj_date = this.objects[index].qj_date;
-                this.returnobjects_.qj_state = this.objects[index].qj_state;
-                this.flag = true;
-            },
-
-            all_agree() {
-                for (let i = 0; i < this.selected.length; i++) {
-                    this.selected[i].qj_state = 1;
-                    this.returnobjects_.wk_number = this.objects[i].wk_number;
-                    this.returnobjects_.qj_reason = this.objects[i].qj_reason;
-                    this.returnobjects_.qj_date = this.objects[i].qj_date;
-                    this.returnobjects_.qj_state = this.objects[i].qj_state
-                }
-                this.flag = true;
-            },
-
-            upload() {
-                var sentArray = [];
-
-                axios.get(
-                    APIUpdateQingjiaState,
-                    {params: {}}
-                ).then(response => {
-                    alert("提交成功" + response.data);
-                }).catch(error => {
-                    alert("提交失败" + error);
-                });
-            },
-
-            get_action() {
-                axios.get(APIGetAllQingjiaByDpNumber, {
-                    params: {
-                        dp_number: sessionStorage.getItem("adminDpNumber")
-                    }
-                }).then(resp => {
-                    this.objects = resp.data.data.result;
-                    this.tempobjects = resp.data.data.result;
-                    for (let i = 0; i < this.tempobjects.length; i++) {
-                        const date = new Date(this.tempobjects[i].qj_date);
-                        const Y = date.getFullYear() + "-";
-                        const M =
-                            (date.getMonth() + 1 < 10
-                                ? "0" + (date.getMonth() + 1)
-                                : date.getMonth() + 1) + "-";
-                        const D = date.getDate();
-                        //this.tempobjects[i].qj_date=(Y+M+D)
-                        this.tempobjects[i]["show_date"] = Y + M + D;
-                    }
-                }).catch(function (error) {
-                    alert(error);
-                });
-            }
-        },
-        // 员工号
-        // 请假时间
-        // 请假理由
-        // 请假状态
-
-        mounted() {
-            this.get_action();
-        },
-        created() {
-            if (sessionStorage.getItem("userID") == null) {
-                alert("非法登录!");
-                this.$router.push({name: 'Login'});
-            }
+    all_agree() {
+      var returnobjects_ = {
+        wk_number: null,
+        qj_date: null,
+        permission: null
+      };
+      for (let i = 0; i < this.selected.length; i++) {
+        if (this.selected[i].qj_state != 0) {
+          continue;
         }
+        this.selected[i].qj_state = 1;
+        returnobjects_.wk_number = this.tempobjects[i].wk_number;
+        returnobjects_.qj_date = this.tempobjects[i].qj_date;
+        returnobjects_.permission = this.tempobjects[i].qj_state;
+        this.sendArray.splice(i, 0, returnobjects_);
+      }
+      this.flag = true;
+    },
+
+    upload() {
+      // var sentArray = [];//array.lenth=tempobject.lenth
+      var sent = this.sendArray;
+      // eslint-disable-next-line
+      var data = {};
+      data["params"] = sent;
+      axios
+        .get(APIUpdateQingjiaState, data)
+        .then(response => {
+          alert("提交成功" + response.data);
+        })
+        .catch(error => {
+          alert("提交失败" + error);
+        });
+    },
+
+    get_action() {
+      axios
+        .get(APIGetAllQingjiaByDpNumber, {
+          params: {
+            dp_number: sessionStorage.getItem("adminDpNumber")
+          }
+        })
+        .then(resp => {
+          this.tempobjects = resp.data.data.result;
+          for (let i = 0; i < this.tempobjects.length; i++) {
+            const date = new Date(this.tempobjects[i].qj_date);
+            const Y = date.getFullYear() + "-";
+            const M =
+              (date.getMonth() + 1 < 10
+                ? "0" + (date.getMonth() + 1)
+                : date.getMonth() + 1) + "-";
+            const D = date.getDate();
+            this.tempobjects[i]["show_date"] = Y + M + D;
+          }
+        })
+        .catch(function(error) {
+          alert(error);
+        });
     }
+  },
+  // 员工号
+  // 请假时间
+  // 请假理由
+  // 请假状态
+
+  mounted() {
+    this.get_action();
+  },
+  created() {
+    if (sessionStorage.getItem("userID") == null) {
+      alert("非法登录!");
+      this.$router.push({ name: "Login" });
+    }
+  }
+};
 </script>
 
 <style scoped>
-    #leaveedit {
-        width: 90%;
-        margin: 25px auto;
-    }
+#leaveedit {
+  width: 90%;
+  margin: 25px auto;
+}
 
-    #qjgl-parallax {
-        position: inherit;
-        height: 100%
-    }
+#qjgl-parallax {
+  position: inherit;
+  height: 100%;
+}
 </style>
